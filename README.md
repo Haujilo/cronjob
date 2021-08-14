f3c54a7d85aee8eead799df9635f8404fe27c914
Luo Yuhao <luoyuhao@haujilo.xyz>
2021-08-14 16:04:59 +0800

    feat: 添加每月自动检查ssl证书和renew操作。

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # 定时任务集合
 
-[![Build Status](https://travis-ci.org/Haujilo/cronjob.svg?branch=main)](https://travis-ci.org/Haujilo/cronjob)
+![Daily work](https://github.com/Haujilo/cronjob/actions/workflows/days.yml/badge.svg)
+![Monthly work](https://github.com/Haujilo/cronjob/actions/workflows/months.yml/badge.svg)
 
 ## 每天
 
 1. 访问备案服务器，维持备案服务器日活
+
+## 每月
+
+1. 检查证书是否过期，过期则重新生成并推送到各存储
---
+++ jobs/renew_cert/run.sh
@@ -0,0 +1,45 @@
+#! /bin/bash -e
+
+RENEW_BEFORE_DAYS=${RENEW_BEFORE_DAYS}
+EMAIL=${EMAIL}
+ARCHIVE_FILE_PASS=${ARCHIVE_FILE_PASS}
+JIANGUOYUN_USER=${JIANGUOYUN_USER}
+JIANGUOYUN_KEY=${JIANGUOYUN_KEY}
+JIANGUOYUN_URI=${JIANGUOYUN_URI}
+export DOMAIN=${DOMAIN}
+export TIMEOUT=${TIMEOUT:-10}
+export DP_Id=${DP_Id}
+export DP_Key=${DP_Key}
+
+CERT_EXPIRE_DATE=`curl --connect-timeout $TIMEOUT -I -s -v https://www.$DOMAIN/ 2>&1 | grep 'expire date:' | cut -d" " -f 5-`
+
+echo "$DOMAIN certificate expire at $CERT_EXPIRE_DATE"
+
+if [ $FORCE_RENEW_CERT ] || [ `date +%s` -ge `date -d "$CERT_EXPIRE_DATE $RENEW_BEFORE_DAYS days ago" +%s` ]; then
+  # install acme.sh
+  git clone https://github.com/acmesh-official/acme.sh.git
+  (cd acme.sh && ./acme.sh --install --accountemail $EMAIL)
+  export ACME_INSTALL_DIR=~/.acme.sh
+
+  # renew cert files
+  $ACME_INSTALL_DIR/acme.sh --register-account -m $EMAIL --set-default-ca --server zerossl
+  $ACME_INSTALL_DIR/acme.sh --issue --dns dns_dp -d $DOMAIN -d "*.$DOMAIN" -d "*.nas.$DOMAIN"
+
+  # package cert files
+  DIR=$DOMAIN.`date +%Y%m%d`
+  mkdir -p $DIR
+  cp -r $ACME_INSTALL_DIR/$DOMAIN/* $DIR/
+  ARCHIVE_FILE=$DOMAIN.zip
+  zip -rj -P $ARCHIVE_FILE_PASS $ARCHIVE_FILE $DIR
+
+  # archive cert files
+  curl --user "$JIANGUOYUN_USER:$JIANGUOYUN_KEY" -T $ARCHIVE_FILE ${JIANGUOYUN_URI}ssl/
+
+  # push to servers
+  scripts=`ls ./lib | sort`
+  for script in $scripts; do
+    ./lib/$script
+  done
+else
+  echo 'Nothing to do.'
+fi
